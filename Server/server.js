47c407435574a9118ea9816b26d1813580ea6eac
robert <robert.299@hotmail.com>
2022-11-26 13:34:26 -0500

    updated http proxy

---
+++ Server/server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+
+app.set("Port", 8000)
+app.use(bodyParser.urlencoded({extended: false }))
+
+app.get('/', (req,res ) => {
+  console.log("server endpoint reached")
+  res.send("Application")
+})
+
+app.post("/auth", (req,res) => {
+  console.log( req.body.name)
+
+  let key = req.body.name
+
+  if( key === "streamKey"){
+    res.status(200).send()
+    return
+  }
+
+  res.status(401).send()
+
+})
+
+app.listen(app.get("Port", ), () => {
+  console.log( "app started on port 8000")
+})
